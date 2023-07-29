0b4ffe0c6b115228a7030989d7232c55a8f703f3
KawyaNilawaka <137979121+KawyaNilawaka@users.noreply.github.com>
2023-07-29 12:07:56 +0530

    Add files via upload

---
+++ Lab03-26.05.2023/Q3/Q3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a value:");
+            int Num=int.Parse(Console.ReadLine());
+
+            int sum = 0, Rem;
+            while (Num>0)
+            {
+                Rem = Num % 10;
+                sum += Rem;
+                Num =Num/ 10;
+            }
+            Console.WriteLine("Sum:" + sum);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab03-26.05.2023/Q4/Q4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int num1=int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i=1; i <= num1; i+=2) 
+            {
+                sum += i;
+            
+            }
+            Console.WriteLine($"Sum of all 0dd numbers from 1 to {num1}={sum}");
+            Console.ReadLine();
+        }
+    }
+}
