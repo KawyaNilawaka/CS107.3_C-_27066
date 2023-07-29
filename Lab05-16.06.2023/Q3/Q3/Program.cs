bcfcee1c5fc377bb4f4971f4e03d3ce8f7043190
KawyaNilawaka <137979121+KawyaNilawaka@users.noreply.github.com>
2023-07-29 14:07:58 +0530

    Add files via upload

---
+++ Lab05-16.06.2023/Q3/Q3/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q3
+{
+    internal class Program
+    {
+        int c = 0;
+        static void Main(string[] args)
+        {
+            CalculateValues objCalulateValues=new CalculateValues();
+
+            int num1;
+            int num2;
+
+            Console.WriteLine("Enter 1 for Addition\n Enter 2 for substraction\n Enter 3 for multiplication\n Enter 4 for division ");
+            Console.WriteLine("Enter your choice:");
+            int choice =int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a two number:");
+            num1= int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a two number:");
+            num2= int.Parse(Console.ReadLine());
+
+            switch (choice) 
+            {
+                case 1:
+                    objCalulateValues.Addition(num1, num2);
+                    Console.WriteLine("Enter your answer:" + c);
+
+                    break;
+                    case 2:
+                    objCalulateValues.substraction(num1, num2);
+                    Console.WriteLine("Enter your answer:"+ c);
+
+                    break;
+                    case 3:
+                    objCalulateValues.Multiplication (num1, num2);
+
+                    break; 
+                    case 4:
+                    objCalulateValues.Division(num1, num2);
+
+                    break;
+                    default:
+                    Console.WriteLine("program Ends!");
+                    break;
+            }
+
+            Console.WriteLine();
+            Console.ReadLine();
+           
+        }
+    }
+}
